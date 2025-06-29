d2f8072be99894a485d6e0b65d8e73fbc4fc6a62
JaeBeen95 <nin2348@gmail.com>
2025-06-29 23:27:06 +0900

    ✨ feat: 초기 컴포넌트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/auth/LoginPage";
+import SignupPage from "./pages/auth/SignupPage";
+import DashboardPage from "./pages/dashboard/DashboardPage";
+
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
